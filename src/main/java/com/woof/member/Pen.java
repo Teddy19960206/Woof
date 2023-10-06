990e3702d2d5b9284acaf163f49575a44fd76d7e
Cindy <Cindy9942@gmail.com>
2023-10-06 15:09:04 +0800

    New: create Pen class

---
+++ src/main/java/com/woof/member/Pen.java
@@ -0,0 +1,23 @@
+package com.woof.member;
+
+public class Pen {
+private String brand;
+private double price;
+
+public Pen() {
+	super();
+}
+public String getBrand() {
+	return brand;
+}
+
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+public void setBrand(String brand) {
+	this.brand = brand;
+}
+}
