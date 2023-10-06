5053b7cf4eb7a7dec30c7deb2b27905eb9b30650
Teddy <bob850206@gmail.com>
2023-10-06 17:55:47 +0800

    test

---
+++ src/main/java/com/woof/trainer/productVo.java
@@ -0,0 +1,23 @@
+package com.woof.trainer;
+
+public class productVo {
+private int price;
+private String ccc;
+private String bbb;
+public productVo(){
+	super();
+}
+public int getPrice() {
+	return price;
+}
+public void setPrice(int price) {
+	this.price = price;
+}
+public String getCcc() {
+	return ccc;
+}
+public void setCcc(String ccc) {
+	this.ccc = ccc;
+}
+
+}
