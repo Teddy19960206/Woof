793b99d8b1a694e16989459f174c50358ed102b9
YuZhang <kup333222@gmail.com>
2023-10-06 17:33:39 +0800

    new class pppp

---
+++ src/main/java/com/woof/trainer/pppp.java
@@ -0,0 +1,21 @@
+package com.woof.trainer;
+
+public class pppp {
+	private int price;
+	private String ccc;
+	public pppp(){
+		super();
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getCcc() {
+		return ccc;
+	}
+	public void setCcc(String ccc) {
+		this.ccc = ccc;
+	}
+}
