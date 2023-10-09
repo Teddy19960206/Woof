a7a937365f3af1307671a18be9f9c7865310c47f
Ziv <trick95710@gmail.com>
2023-10-09 23:10:14 +0800

    add GroupCourseorderService

--- src/main/java/com/woof/groupcourseorder/service/GroupCourseOrderService.java
+++ src/main/java/com/woof/groupcourseorder/service/GroupCourseOrderService.java
@@ -1,5 +1,14 @@
 package com.woof.groupcourseorder.service;
 
+import com.woof.groupcourseorder.model.*;
+
 public class GroupCourseOrderService {
+	
+	private GroupCourseOrderDAO groupCourseOrderDAO;
+	
+	public GroupCourseOrderService() {
+		groupCourseOrderDAO = new GroupCourseOrderDAOImpl();
+		
+	}
 
 }
