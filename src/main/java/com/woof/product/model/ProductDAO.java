d890e2e443e286793b099169b48d49b5997a3758
yuyao <asas12365414@gmail.com>
2023-10-09 18:09:32 +0800

    修改productVO並做了productDAO_interface與productDAO

---
+++ src/main/java/com/woof/product/model/ProductDAO.java
@@ -0,0 +1,12 @@
+package com.woof.product.model;
+
+import java.util.List;
+
+// DAO Interface
+public interface ProductDAO {
+    public void insert(ProductVO productVO);
+    public void update(ProductVO productVO);
+    public void delete(Integer prodNo);
+    public ProductVO findByProdNo(Integer prodNo);
+    public List<ProductVO> getAll();
+}
