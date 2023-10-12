1f663fc17cb4648a1ae813f77912a9d1ac27ea29
Cindy <Cindy9942@gmail.com>
2023-10-12 14:57:39 +0800

    add functionpermission hibernateVO

--- src/main/java/com/woof/functionpermission/model/FunctionPermissionVO.java
+++ src/main/java/com/woof/functionpermission/model/FunctionPermissionVO.java
@@ -44,3 +44,59 @@ public class FunctionPermissionVO implements java.io.Serializable {
 		return "FunctionPermissionVO [funcNo=" + funcNo + ", funcName=" + funcName + "]";
 	}
 }
+
+//package com.woof.functionpermission.model;
+//
+//import java.util.Objects;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "function_permission")
+//public class FunctionPermissionVO implements java.io.Serializable {
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@Column(name = "FUNC_NO")
+//	private Integer funcNo;
+//
+//	@Column(name = "FUNC_NAME")
+//	private Integer funcName;
+//
+//	public Integer getFuncNo() {
+//		return funcNo;
+//	}
+//
+//	public void setFuncNo(Integer funcNo) {
+//		this.funcNo = funcNo;
+//	}
+//
+//	public Integer getFuncName() {
+//		return funcName;
+//	}
+//
+//	public void setFuncName(Integer funcName) {
+//		this.funcName = funcName;
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		return Objects.hash(funcName, funcNo);
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		FunctionPermissionVO other = (FunctionPermissionVO) obj;
+//		return Objects.equals(funcName, other.funcName) && Objects.equals(funcNo, other.funcNo);
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "FunctionPermissionVO [funcNo=" + funcNo + ", funcName=" + funcName + "]";
+//	}
+//}
