786539f7edddea8567a73654589c8ca80dcb9cc4
Teddy <bob850206@gmail.com>
2023-10-16 18:12:34 +0800

    Revert "新增各表格DAO"
    This reverts commit b017de76436614b59d68ac19de525ada31f8a9e7.

--- src/main/java/com/woof/productcategory/dao/ProductCategoryDAO.java
+++ src/main/java/com/woof/productcategory/dao/ProductCategoryDAO.java
@@ -1,24 +1,13 @@
 package com.woof.productcategory.dao;
 
 import java.util.List;
-import java.util.Map;
 
 import com.woof.productcategory.entity.ProductCategory;
 
 public interface ProductCategoryDAO {
-	int insert(ProductCategory productCategory);
-
-	int update(ProductCategory productCategory);
-
-	int delete(Integer prodCatNo);
-
-	ProductCategory findByProdCatNo(Integer prodCatNo);
-
-	List<ProductCategory> getAll();
-
-	List<ProductCategory> getByCompositeQuery(Map<String, String> map);
-
-	List<ProductCategory> getAll(int currentPage);
-
-	long getTotal();
+    public void insert(ProductCategory productCategoryVO);
+    public void update(ProductCategory productCategoryVO);
+    public void delete(Integer prodCatNo);
+    public ProductCategory findByProdCatNo(Integer prodCatNo);
+    public List<ProductCategory> getAll();
 }
