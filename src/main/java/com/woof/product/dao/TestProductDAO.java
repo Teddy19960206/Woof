c663aedd708b4c9fe2b55cb5368683fac4ce3be7
yuyao <asas12365414@gmail.com>
2023-10-19 21:10:59 +0800

    新增jsp 修改servlet service

--- src/main/java/com/woof/product/dao/TestProductDAO.java
+++ src/main/java/com/woof/product/dao/TestProductDAO.java
@@ -5,7 +5,7 @@ import com.woof.product.entity.Product;
 
 public class TestProductDAO {
 		public static void main(String[] args) {
-			ProductDAO1 productDAO = new ProductDAOImpl1();
+			ProductDAO productDAO = new ProductDAOImpl();
 			Product productVO = new Product();
 
 //			// 新增
