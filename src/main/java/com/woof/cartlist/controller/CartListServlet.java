57d86681440a9b90526235aae47c447e1c7468ab
Jessica <emi81615@gmail.com>
2023-11-05 13:52:40 +0800

    update cartlist

--- src/main/java/com/woof/cartlist/controller/CartListServlet.java
+++ src/main/java/com/woof/cartlist/controller/CartListServlet.java
@@ -1,74 +1,88 @@
 package com.woof.cartlist.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import redis.clients.jedis.Jedis;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import redis.clients.jedis.Jedis;
 
 @WebServlet("/cartlist")
 public class CartListServlet extends HttpServlet {
-	// 到時候加入會員使用複合鍵的Map
-	// Map<String, Map<String, Integer>> cart = new HashMap<>();
-
-	private Map<String, Integer> cart = new HashMap<>();
-	private Jedis jedis = new Jedis("localhost", 6379); // 假設 Redis 服務器運行在本機的 6379 端口
+	private Jedis jedis = new Jedis("localhost", 6379);
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		String action = request.getParameter("action");
 
 		if ("add".equals(action)) {
+//			HttpSession session = request.getSession();
+//			String memNo = (String) session.getAttribute("memNo"); // 假設會員編號已經存儲在會話中
+			String memNo = "member1";
+//			if (memNo == null) {
+//				// 處理未登入狀態
+//				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "用戶未登入");
+//				return;
+//			}
+
+			// 從 Redis 獲取當前購物車
+			String cartJson = jedis.get(memNo);
+			List<Map<String, Object>> cart;
+			if (cartJson != null) {
+				cart = new Gson().fromJson(cartJson, new TypeToken<List<Map<String, Object>>>() {
+				}.getType());
+			} else {
+				cart = new ArrayList<>();
+			}
+
 			// 添加商品到購物車
 			String prodNo = request.getParameter("prodNo");
 			String prodName = request.getParameter("prodName");
 			double prodPrice = Double.parseDouble(request.getParameter("prodPrice"));
-			// 假設您已經有會員編號 memNo
-            String memNo = "member1"; // 您需要從某處獲取這個會員編號"someMemberId"
-			
-			
-			// 從登入的會員獲取會員帳號
-			// String memNo = request.getParameter("memNo");
+			boolean exists = false;
 
-			// 檢查購物車是否已經包含相同的商品，如果是，增加數量
-			if (cart.containsKey(prodName)) {
-				cart.put(prodName, cart.get(prodName) + 1);
-			} else {
-				cart.put(prodName, 1);
+			// 檢查購物車是否已經包含該商品
+			for (Map<String, Object> item : cart) {
+				if (prodNo.equals(item.get("prodNo"))) {
+					int quantity = (int) Double.parseDouble(item.get("quantity").toString());
+					item.put("quantity", quantity + 1); // 增加數量
+					exists = true;
+					break;
+				}
 			}
 
-			System.out.println("購物車有" + cart);
+			if (!exists) {
+				Map<String, Object> newItem = new HashMap<>();
+				newItem.put("prodNo", prodNo);
+				newItem.put("prodName", prodName);
+				newItem.put("quantity", 1);
+				newItem.put("prodprice", prodPrice);
+				cart.add(newItem);
+			}
 
-			response.setContentType("text/html; charset=UTF-8");
-			// 更新購物車中的數量
-			int totalItems = cart.values().stream().mapToInt(Integer::intValue).sum();
+			// 將更新後的購物車轉換為JSON字符串並存回Redis
+			cartJson = new Gson().toJson(cart);
+			jedis.set(memNo, cartJson);
 
-			System.out.println("一共有" + totalItems + "件");
-
-			Gson gson = new Gson();
-			String totalItemsJson = gson.toJson(totalItems);
-			String cartJson = gson.toJson(cart);
-			
-            // 將購物車資訊存入 Redis
-            jedis.set(memNo, cartJson);
-			
+			// 設置響應
 			response.setContentType("application/json");
 			response.setCharacterEncoding("UTF-8");
-			// 傳回到data
-			response.getWriter().write(totalItemsJson);
-
+			response.getWriter().write(cartJson); // 傳回購物車的JSON
 		}
-
 	}
 
 	@Override
 	public void destroy() {
-		super.destroy();
+
 		jedis.close(); // 確保在 Servlet 被銷毀時關閉 Jedis 客戶端
+		super.destroy();
 	}
 }
