693db03c313129200266819cb422c5df9eca5ee1
Ziv <you@example.com>
2023-11-19 16:00:13 +0800

    add chat

---
+++ src/main/java/com/woof/chat/model/Chat.java
@@ -0,0 +1,5 @@
+package com.woof.chat.model;
+
+public class Chat {
+
+}
---
+++ src/main/webapp/backend/backChat/js/backChat.js
@@ -0,0 +1,8 @@
+// document.getElementById('send-button').addEventListener('click', function() {
+//     var message = document.getElementById('message-input').value;
+//     // 在這裡添加發送訊息的代碼
+//     console.log("發送訊息:", message);
+//     document.getElementById('message-input').value = ''; // 清空輸入框
+// });
+//
+// // 在這裡添加更多的JavaScript代碼，例如處理會員列表點擊事件等
