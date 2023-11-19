fb81dba2cf842dabb22ea90e3fa5bf09c5bfba04
Ziv <you@example.com>
2023-11-20 02:42:38 +0800

    add chat to webSocket

--- src/main/java/com/woof/chat/controller/ChatServlet.java
+++ src/main/java/com/woof/chat/controller/ChatServlet.java
@@ -2,30 +2,30 @@ package com.woof.chat.controller;
 
 import com.google.gson.Gson;
 import com.woof.chat.model.State;
+import com.woof.chat.model.ChatMessage;
 
 import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-@ServerEndpoint("/frontEnd/{memberName}")
+@ServerEndpoint("/chat/{user}")
 public class ChatServlet {
 
     private static Map<String , Session> sessionsMap = new ConcurrentHashMap<>();
     Gson gson = new Gson();
 
     @OnOpen
-    public void onOpen(@PathParam("memberName") String memberName, Session userSession) throws IOException {
-        /* save the new user in the map */
-        sessionsMap.put(memberName, userSession);
+    public void onOpen(@PathParam("user") String userName, Session userSession) throws IOException {
+
+        sessionsMap.put(userName, userSession);
         /* Sends all the connected users to the new user */
         Set<String> memberNames = sessionsMap.keySet();
-        State stateMessage = new State("open", memberName, memberNames);
+        State stateMessage = new State("open", userName, memberNames);
         String stateMessageJson = gson.toJson(stateMessage);
         Collection<Session> sessions = sessionsMap.values();
         for (Session session : sessions) {
@@ -35,17 +35,19 @@ public class ChatServlet {
             }
         }
 
+
+
         String text = String.format("Session ID = %s, connected; userName = %s%nusers: %s", userSession.getId(),
-                memberName, memberNames);
+                userName, memberNames);
         System.out.println(text);
     }
 
     @OnMessage
     public void onMessage(Session userSession, String message) {
-//        ChatMessage chatMessage = gson.fromJson(message, ChatMessage.class);
-//        String sender = chatMessage.getSender();
-//        String receiver = chatMessage.getReceiver();
-//
+        ChatMessage chatMessage = gson.fromJson(message, ChatMessage.class);
+        String sender = chatMessage.getSender();
+        String receiver = chatMessage.getReceiver();
+
 //        if ("history".equals(chatMessage.getType())) {
 //            List<String> historyData = JedisHandleMessage.getHistoryMsg(sender, receiver);
 //            String historyMsg = gson.toJson(historyData);
@@ -56,14 +58,14 @@ public class ChatServlet {
 //                return;
 //            }
 //        }
-//
-//
-//        Session receiverSession = sessionsMap.get(receiver);
-//        if (receiverSession != null && receiverSession.isOpen()) {
-//            receiverSession.getAsyncRemote().sendText(message);
-//            userSession.getAsyncRemote().sendText(message);
+
+
+        Session receiverSession = sessionsMap.get(receiver);
+        if (receiverSession != null && receiverSession.isOpen()) {
+            receiverSession.getAsyncRemote().sendText(message);
+            userSession.getAsyncRemote().sendText(message);
 //            JedisHandleMessage.saveChatMessage(sender, receiver, message);
-//        }
+        }
         System.out.println("Message received: " + message);
     }
 
@@ -74,27 +76,27 @@ public class ChatServlet {
 
     @OnClose
     public void onClose(Session userSession, CloseReason reason) {
-//        String userNameClose = null;
-//        Set<String> userNames = sessionsMap.keySet();
-//        for (String userName : userNames) {
-//            if (sessionsMap.get(userName).equals(userSession)) {
-//                userNameClose = userName;
-//                sessionsMap.remove(userName);
-//                break;
-//            }
-//        }
-//
-//        if (userNameClose != null) {
-//            State stateMessage = new State("close", userNameClose, userNames);
-//            String stateMessageJson = gson.toJson(stateMessage);
-//            Collection<Session> sessions = sessionsMap.values();
-//            for (Session session : sessions) {
-//                session.getAsyncRemote().sendText(stateMessageJson);
-//            }
-//        }
+        String userNameClose = null;
+        Set<String> userNames = sessionsMap.keySet();
+        for (String userName : userNames) {
+            if (sessionsMap.get(userName).equals(userSession)) {
+                userNameClose = userName;
+                sessionsMap.remove(userName);
+                break;
+            }
+        }
 
-//        String text = String.format("session ID = %s, disconnected; close code = %d%nusers: %s", userSession.getId(),
-//                reason.getCloseCode().getCode(), userNames);
-//        System.out.println(text);
+        if (userNameClose != null) {
+            State stateMessage = new State("close", userNameClose, userNames);
+            String stateMessageJson = gson.toJson(stateMessage);
+            Collection<Session> sessions = sessionsMap.values();
+            for (Session session : sessions) {
+                session.getAsyncRemote().sendText(stateMessageJson);
+            }
+        }
+
+        String text = String.format("session ID = %s, disconnected; close code = %d%nusers: %s", userSession.getId(),
+                reason.getCloseCode().getCode(), userNames);
+        System.out.println(text);
     }
 }
--- src/main/webapp/backend/backChat/js/backChat.js
+++ src/main/webapp/backend/backChat/js/backChat.js
@@ -1,8 +1,154 @@
-// document.getElementById('send-button').addEventListener('click', function() {
-//     var message = document.getElementById('message-input').value;
-//     // 在這裡添加發送訊息的代碼
-//     console.log("發送訊息:", message);
-//     document.getElementById('message-input').value = ''; // 清空輸入框
-// });
-//
-// // 在這裡添加更多的JavaScript代碼，例如處理會員列表點擊事件等
+let host = window.location.host;
+let pathFor = window.document.location.pathname;
+let projectName = pathFor.substring( 0 , pathFor.substring(1).indexOf("/")+1);
+let self = "admin";
+let myPoint = `/chat/${self}`;
+let endPoint = `ws://` + host + projectName + myPoint;
+const inputHistory = document.getElementById("input-history");
+const inputText = document.getElementById("inputText");
+const chatHeader = document.getElementById("chat-header");
+const chatList = document.getElementById("chat-list");
+// receiver 接收
+
+// sender 傳送
+
+
+let member ;
+
+addEventListener("DOMContentLoaded" , function (){
+    connect();
+})
+$(window).on('beforeunload', function() {
+    disconnect();
+});
+
+
+let websocket;
+function connect(){
+    websocket = new WebSocket(endPoint);
+
+    websocket.onopen = function (event){
+        console.log("連線成功");
+    }
+
+    websocket.onmessage = function (event){
+        const jsonObj = JSON.parse(event.data);
+        console.log(jsonObj);
+        if ("open" === jsonObj.type) {
+            refreshFriendList(jsonObj);
+        }else if ("chat"){
+            if (member === jsonObj.sender){
+                addMessage(jsonObj);
+            }else{
+
+            }
+        }
+    }
+
+    websocket.onclose = function (event) {
+
+    }
+
+    websocket.onerror = function (event){
+
+    }
+}
+
+function disconnect(){
+    websocket.close();
+}
+
+
+function sendMessage(message) {
+
+    let jsonObj = {
+        "type" : "chat",
+        "sender" : self,
+        "receiver" : member,
+        "message" : message
+    };
+    websocket.send(JSON.stringify(jsonObj));
+    addMessage(jsonObj)
+    inputText.value = "";
+    inputText.focus();
+    scrollToBottom();
+}
+
+function addMessage(jsonObj){
+    $("#input-history")
+        .append(`<li class="clearfix">
+                    <div class="message-data ${jsonObj.receiver == self ? '' : 'text-right'}">
+                      <span class="message-data-time">${new Date().toLocaleTimeString()}</span>
+                    </div>         
+                    <div class="message ${jsonObj.receiver == self ? 'my-message' : 'other-message float-right'}">${jsonObj.message}</div>
+                 </li>`);
+    scrollToBottom();
+}
+
+$(".fa-send").on("click" , function (){
+    if (inputText.value.trim() !== "" ){
+        sendMessage(inputText.value);
+    }
+
+});
+
+
+$("#inputText").on("keyup" , function (e){
+    if (inputText.value.trim() !== "" && e.keyCode === 13){
+        sendMessage(inputText.value);
+    }
+})
+
+
+function scrollToBottom() {
+    let div = document.getElementById("chat-history");
+    div.scrollTop = div.scrollHeight;
+}
+
+// 有好友上線或離線就更新列表
+function refreshFriendList(jsonObj) {
+    let members = jsonObj.users;
+    chatList.innerHTML = '';
+    console.log(members.length)
+    for (let i = 0; i < members.length; i++) {
+        if (members[i] === self) { continue; }
+        appendMemberList(membersList(members[i]));
+    }
+    // addListener();
+}
+
+function appendMemberList(message){
+    chatList.innerHTML += message;
+}
+
+function membersList(member){
+    return `<li class="clearfix li-chat" data-id="${member}">
+              <img src="${projectName}/DBPngReader?action=member&id=${member}" alt="avatar">
+              <div class="about">
+                <div class="name">${member}</div>
+                <div class="status"> <i class="fa fa-circle online"></i> on-line </div>
+              </div>
+            </li>`;
+}
+
+function memberHeader(member){
+    return `<div class="row">
+              <div class="col-lg-6">
+                <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
+                  <img src="${projectName}/DBPngReader?action=member&id=${member}" alt="avatar">
+                </a>
+                <div class="chat-about">
+                  <h6 class="m-b-0">${member}</h6>
+                </div>
+              </div>
+            </div>`;
+}
+
+$(document).on("click" , "li.li-chat" , function (){
+    member = $(this).data("id");
+    chatHeader.innerHTML = memberHeader(member);
+    inputHistory.innerHTML = "";
+//  更換聊天室
+    $("#inputText").prop("disabled" , false);
+
+})
