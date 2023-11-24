5ea403821094d78a9db57752f889c88e3d5f1a61
Jessica <emi81615@gmail.com>
2023-11-24 19:20:02 +0800

    update

--- src/main/webapp/frontend/cartlist/js/cart.js
+++ src/main/webapp/frontend/cartlist/js/cart.js
@@ -103,7 +103,7 @@ $(document).on("click", "#cart-icon", function() {
 					title: "購物車是空的，請去商城逛逛~",
 				});
 
-				window.location.href = `${projectCart}/shopHome.html`;
+//				window.location.href = `${projectCart}/shopHome.html`;
 				return;
 			}
 
@@ -118,7 +118,7 @@ $(document).on("click", "#cart-icon", function() {
 				html += `<tr>
 						    <td class="text-center align-middle">${item.prodName}</td>
 						    <td class="text-center align-middle">
-						        <img src="${projectName}/productImage/${item.prodNo}" style="width: 100px; height: 100px;" onerror="this.onerror=null; this.src='${projectName}/fallback-image.jpg';"/>
+						        <img src="${projectCart}/productImage/${item.prodNo}" style="width: 100px; height: 100px;" onerror="this.onerror=null; this.src='${projectCart}/fallback-image.jpg';"/>
 						    </td>
 						    <td class="text-center align-middle">${item.quantity}</td>
 						    <td class="text-center align-middle">NT$${itemTotal}</td>
